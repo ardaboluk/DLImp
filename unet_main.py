Added softmax to Vanilla UNet. Fixed test inputs in unet_main. Vanilla UNet working now. Gotta train and test, though.

unet_main.py
@@ -9,6 +9,7 @@ from tensorflow.keras import layers
 
 import PIL
 from PIL import Image, ImageOps
+from tensorflow.keras.preprocessing.image import load_img
 
 import random
 
@@ -59,16 +60,16 @@ plt.show()
 # arda: additionally get test set
 #val_samples = 980
 #test_samples = 20
-val_samples = 5000
-test_samples = 1000
+val_samples = 7000
+test_samples = 20
 random.Random(1337).shuffle(input_img_paths)
 random.Random(1337).shuffle(target_img_paths)
 train_input_img_paths = input_img_paths[:-(val_samples+test_samples)]
 train_target_img_paths = target_img_paths[:-(val_samples+test_samples)]
 val_input_img_paths = input_img_paths[-(val_samples+test_samples):-test_samples]
 val_target_img_paths = target_img_paths[-(val_samples+test_samples):-test_samples]
-test_input_img_paths = input_img_paths[:-test_samples]
-test_target_img_paths = target_img_paths[:-test_samples]
+test_input_img_paths = input_img_paths[-test_samples:]
+test_target_img_paths = target_img_paths[-test_samples:]
 
 # Instantiate data Sequences for each split
 train_gen = OxfordPets(
@@ -82,7 +83,7 @@ unet_model.summary()
 keras.utils.plot_model(unet_model, show_shapes=True)
 
 print("Compiling the model..")
-unet_model.compile(optimizer="adam", loss=keras.losses.SparseCategoricalCrossentropy(), metrics="sparse_categorical_accuracy", run_eagerly=False)
+unet_model.compile(optimizer="rmsprop", loss=keras.losses.SparseCategoricalCrossentropy(), metrics="sparse_categorical_accuracy", run_eagerly=False)
 
 #checkpoint_dir = "./train_ckpt/cp-{epoch:04d}.ckpt"
 #cp_callback = keras.callbacks.ModelCheckpoint(filepath=checkpoint_dir, save_weights_only=True, verbose=1)
@@ -95,6 +96,28 @@ print("Training..")
 history = unet_model.fit(train_gen, validation_data=val_gen, epochs=epochs_num)
 
 print("Testing..")
-results = unet_model.evaluate(test_gen)
+results = unet_model.predict(test_gen)
 
-print(results[0].shape)
+def display_mask(pred_results_list, i):
+    """Quick utility to display a model's prediction."""
+    mask = np.argmax(pred_results_list[i], axis=-1)
+    mask = np.expand_dims(mask, axis=-1)
+    imag = PIL.ImageOps.autocontrast(keras.preprocessing.image.array_to_img(mask))
+    plt.imshow(imag)
+    plt.show()
+
+
+# Display results for test image #10
+test_img_ind = 10
+
+# Display input image
+plt.imshow(Image.open(test_input_img_paths[test_img_ind]))
+plt.show()
+
+# Display ground-truth target mask
+img = PIL.ImageOps.autocontrast(load_img(test_target_img_paths[test_img_ind]))
+plt.imshow(img)
+plt.show()
+
+# Display mask predicted by our model
+display_mask(results, test_img_ind)
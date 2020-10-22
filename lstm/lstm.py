19fd1d34a740f081e6f4c1cb8b7dbb22ce26b427
Salih Arda Bölük <sarda.boluk@stm.com.tr>
2020-10-22 17:20:23 +0300

    Working lstm model and main file commit.

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -1,6 +1,13 @@
 
-import tensorflow
-import tensorflow.keras as keras
+import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import layers
 
-def lstm_model(num_classes = 2):
+def lstm_model(input_shape, num_classes = 2):
+    inputs = keras.layers.Input(shape = input_shape)
+
+    lstm_out = keras.layers.LSTM(32)(inputs)
+    outputs = keras.layers.Dense(num_classes)(lstm_out)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
